5674134b2c0c55ace53e2b50d7ce2449e23a32a1
Natan Brosztein <code@natanbro.com>
2021-09-21 22:34:57 +0300

    Changed the install file to use the Plug vim plugin manager instead of Vumdle

--- install.sh
+++ install.sh
@@ -19,7 +19,7 @@ fi
 ln -s $PWD/.tmux.conf ~/.tmux.conf
 ###
 if [ -f ~/.bashrc ] ; then
-	echo Renaming existing ~/.bash_profile to ~/.bashrc.back
+	echo Renaming existing ~/.bashrc to ~/.bashrc.back
 	cp ~/.bashrc ~/.bashrc.back
 	rm ~/.bashrc
 fi
@@ -31,58 +31,29 @@ if [ -f ~/.bash_profile ] ; then
 	rm ~/.bash_profile
 fi
 ln -s $PWD/.bash_profile ~/.bash_profile
-###
-##if [ -f ~/.vimrc ] ; then
-##	echo Renaming existing ~/.vimrc to ~/.vimrc.back
-##	cp ~/.vimrc ~/.vimrc.back
-##	rm ~/.vimrc
-##fi
-##ln -s $PWD/vimrc ~/.vimrc
-###
-##if [ -f ~/.vimrc_bundles ] ; then
-##	echo Renaming existing ~/.vimrc_bundles to ~/.vimrc_bundles.back
-##	cp ~/.vimrc_bundles ~/.vimrc_bundles.back
-##	rm ~/.vimrc_bundles
-##fi
-##ln -s $PWD/vimrc_bundles ~/.vimrc_bundles
-###
-##if [ -f ~/.vimrc_bundles_pre ] ; then
-##	echo Renaming existing ~/.vimrc_bundles_pre to ~/.vimrc_bundles_pre.back
-##	cp ~/.vimrc_bundles_pre ~/.vimrc_bundles_pre.back
-##	rm ~/.vimrc_bundles_pre
-##fi
-##ln -s $PWD/vimrc_bundles_pre ~/.vimrc_bundles_pre
-###
-###
-##if [ -f ~/abbreviations.vim ] ; then
-##	echo Renaming existing ~/abbreviations.vim to ~/abbreviations.vim.back
-##	cp ~/abbreviations.vim ~/abbreviations.vim.back
-##	rm ~/abbreviations.vim
-##fi
-##ln -s $PWD/abbreviations.vim ~/abbreviations.vim
-###
-##if [ -L ~/.vim ] ; then
-##	echo Renaming existing ~/.vim to ~/.vim.back
-##	mv ~/.vim ~/.vim.back
-###	rm ~/.vim
-##fi
-##ln -s $PWD/vim ~/.vim
-###
-##if [ -f ~/.vimrc_bundles_pre ] ; then
-##	echo Renaming existing ~/.vimrc_bundles_pre to ~/.vimrc_bundles_pre.back
-##	cp ~/.vimrc_bundles_pre ~/.vimrc_bundles_pre.back
-##	rm ~/.vimrc_bundles_pre
-##fi
-##ln -s $PWD/vimrc_bundles_pre ~/.vimrc_bundles_pre
-###
-##if [ -d ~/.vim/bundles ] ; then
-##	echo Removing ~/.vim/bundles
-##	rm -Rf ~/.vim/bundles
-##	rmdir  ~/.vim/bundles
-##fi
-##mkdir ~/.vim/bundles
-###
-##git clone https://github.com/VundleVim/Vundle.vim.git ~/.vim/bundle/Vundle.vim
+#
+if [ -f ~/.vimrc ] ; then
+	echo Renaming existing ~/.vimrc to ~/.vimrc.back
+	cp ~/.vimrc ~/.vimrc.back
+	rm ~/.vimrc
+fi
+ln -s $PWD/vimrc ~/.vimrc
+#
+if [ -f ~/abbreviations.vim ] ; then
+	echo Renaming existing ~/abbreviations.vim to ~/abbreviations.vim.back
+	cp ~/abbreviations.vim ~/abbreviations.vim.back
+	rm ~/abbreviations.vim
+fi
+ln -s $PWD/abbreviations.vim ~/abbreviations.vim
+#
+if [ -L ~/.vim ] ; then
+	echo Renaming existing ~/.vim to ~/.vim.back
+	mv ~/.vim ~/.vim.back
+#	rm ~/.vim
+fi
+ln -s $PWD/vim ~/.vim
+#
 ##git clone https://github.com/tmux-plugins/tpm ~/.tmux/plugins/tpm
-##
+curl -fLo ~/.vim/autoload/plug.vim --create-dirs \
+	    https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
 ##
